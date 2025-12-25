2a5d1f7dbd4bc698fbbd39f4706f6b62c14f8a27
MinePY <qwerty123sdz@gmail.com>
2025-12-25 23:12:36 +0100

    created main.css

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", ()=>{document.body.classList.add("loaded");});
+document.querySelectorAll('a[href^="#"]').forEach(link=>{
+  link.addEventListener("click",e=>{
+    e.preventDefault();
+    const target=document.querySelector(link.getAttribute("href"));
+    if(target){target.scrollIntoView({behavior:"smooth"});}
+  });
+});
